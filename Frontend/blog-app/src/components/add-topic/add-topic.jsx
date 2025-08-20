04aaa927c4792794beffa4dd5b091205470c66b9
Sameer <littlesameer2020@gmail.com>
2025-08-20 09:29:18 +0530

    Home-page-commit

--- Frontend/blog-app/src/components/add-topic/add-topic.jsx
+++ Frontend/blog-app/src/components/add-topic/add-topic.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
 
 
 const AddTopic = () => {
@@ -13,11 +11,14 @@ const AddTopic = () => {
         updateDescription(event.target.value);
     }
     return (
-        <div>
-            <form>
+        <div className='add-topic-container'>
+            <form className='add-topic-form-container'>
+                <div className='label-container'>
                 <label htmlFor="topic" className="add-topic-title-label">Title :</label>
                 <input type="text" className="add-topic-title-input" value={title} onChange={changeTitle} id="topic"/>
+                </div>
                 <label htmlFor="description" className="add-topic-title-description">Description</label>
+                <textarea rows={20} cols={20} />
             </form>
         </div>
     )
